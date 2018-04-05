bcd2fa133b8c484c767efdbf5740026128632096
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-04-05 13:09:41 +0900

    AnimationTimer

--- AnimationTimer/ViewController.swift
+++ AnimationTimer/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    
+    var counter = 1
+    var myTimer = Timer()
+    var play = 0
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var Index: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Index.text = String(counter)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btSt(_ sender: Any) {
+        if play == 0 {
+            myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            button.setTitle("stop", for : .normal)
+            play = 1
+        } else {
+            myTimer.invalidate()
+            button.setTitle("start", for : .normal)
+            play = 0
+            
+        }
+    }
+    
+    
+    
+    
+    @objc func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter = counter + 1
+        }
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        Index.text = String(counter)
+    }
+    
 }
-
