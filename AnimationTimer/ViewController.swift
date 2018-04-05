b6c235e9c9d99bb2d7c4e537e7188129ab7dc759
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-04-05 13:05:44 +0900

    Initial Commit

---
+++ AnimationTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationTimer
+//
+//  Created by D7703_16 on 2018. 4. 5..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
